ac3a215330bd38a9edbbd4b45ce7b1236b4279fe
Narda-github <nardaarandria@gmail.com>
2024-04-13 21:26:07 +0300

    création de l'entité CompteBancaire

---
+++ src/main/java/mg/itu/narda/tpbanque/entity/CompteBancaire.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.narda.tpbanque.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Narda <mg.itu.narda>
+ */
+@Entity
+
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private int solde;
+
+    public CompteBancaire() {
+    }
+
+    public CompteBancaire(String nom, int solde) {
+        this.nom = nom;
+        this.solde = solde;
+    }
+
+    public void deposer(int montant) {
+        solde += montant;
+    }
+
+    public void retirer(int montant) {
+        if (montant < solde) {
+            solde -= montant;
+        } else {
+            solde = 0;
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    /* public void setId(Long id) {
+    this.id = id;
+    }*/
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.itu.narda.tpbanque.entity.CompteBancaire[ id=" + id + " ]";
+    }
+
+}
