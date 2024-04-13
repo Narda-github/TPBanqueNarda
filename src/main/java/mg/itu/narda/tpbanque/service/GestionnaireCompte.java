5feb01818c325f93fbac40849987570b851e5abc
Narda-github <nardaarandria@gmail.com>
2024-04-13 21:38:46 +0300

    Écriture d'un bean CDI GestionnaireCompte 	-Déclaration de la source de données 	-Modification fichier persistence.xml

---
+++ src/main/java/mg/itu/narda/tpbanque/service/GestionnaireCompte.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.narda.tpbanque.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ *
+ * pour gérer les comptes bancaires
+ *
+ * @author Narda <mg.itu.narda>
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "root123!", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@RequestScoped
+public class GestionnaireCompte {
+
+}
