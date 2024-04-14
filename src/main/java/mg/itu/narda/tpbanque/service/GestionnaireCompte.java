b38c22d3d44c2af474704abe123d588b34ab3fb8
Narda-github <nardaarandria@gmail.com>
2024-04-14 22:59:42 +0300

    Déposer ou retirer de l'argent sur un compte

--- src/main/java/mg/itu/narda/tpbanque/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/narda/tpbanque/service/GestionnaireCompte.java
@@ -45,13 +45,37 @@ public class GestionnaireCompte {
         return em.find(CompteBancaire.class, id);
     }
 
+    /**
+     * Dépôt d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.deposer(montant);
+        update(compteBancaire);
+    }
+
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.retirer(montant);
+        update(compteBancaire);
+    }
+
     @Transactional
     public void transferer(CompteBancaire source, CompteBancaire destination,
             int montant) {
-        source.retirer(montant);
-        destination.deposer(montant);
-        update(source);
-        update(destination);
+        retirer(source, montant);
+        deposer(destination, montant);
+        /*update(source);
+        update(destination);*/
     }
 
     @Transactional
