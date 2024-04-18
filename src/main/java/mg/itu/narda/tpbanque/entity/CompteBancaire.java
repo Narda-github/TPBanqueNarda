969851a6755b45df112b281688ea1965dc7caa8e
Narda-github <nardaarandria@gmail.com>
2024-04-18 12:35:28 +0300

    Creation d'une classe entité OperationBancaire Ajout de l'association dans la classe entité CompteBancaire Modification de  l'entité CompteBancaire TP5

--- src/main/java/mg/itu/narda/tpbanque/entity/CompteBancaire.java
+++ src/main/java/mg/itu/narda/tpbanque/entity/CompteBancaire.java
@@ -4,13 +4,18 @@
  */
 package mg.itu.narda.tpbanque.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -31,6 +36,12 @@ public class CompteBancaire implements Serializable {
     private Long id;
     private String nom;
     private int solde;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<OperationBancaire> operations = new ArrayList<>();
+
+    public List<OperationBancaire> getOperations() {
+        return operations;
+    }
 
     public CompteBancaire() {
     }
@@ -38,15 +49,19 @@ public class CompteBancaire implements Serializable {
     public CompteBancaire(String nom, int solde) {
         this.nom = nom;
         this.solde = solde;
+        operations.add(new OperationBancaire("Création du compte", solde));
     }
 
     public void deposer(int montant) {
         solde += montant;
+        operations.add(new OperationBancaire("Crédit", montant));
     }
 
     public void retirer(int montant) {
         if (montant < solde) {
             solde -= montant;
+            int debit = montant * -1;
+            operations.add(new OperationBancaire("Débit", debit));
         } else {
             solde = 0;
         }
